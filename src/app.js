755fea4f4fe225a17363b89756b003e3df853b75
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2024-04-26 23:06:37 +0300

    +

--- src/app.js
+++ src/app.js
@@ -7,42 +7,50 @@ const server = http.createServer((request, response) => {
   const url = new URL(request.url, ipAddress);
   const userName = url.searchParams.get("hello");
 
-  if (request.url === "/?users") {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
-    response.write(getUsers());
+  if (request.url.length > 1) {
+    if (request.url === "/?users") {
+      response.statusCode = 200;
+      response.statusMessage = "OK";
+      response.header = "Content-Type: application/json";
+      response.write(getUsers());
+      response.end();
+
+      return;
+    }
+
+    if (request.url === "/?hello") {
+      response.statusCode = 400;
+      response.statusMessage = "OK";
+      response.header = "Content-Type: text/plain";
+      response.write("Enter a name");
+      response.end();
+
+      return;
+    }
+
+    if (userName) {
+      response.statusCode = 200;
+      response.statusMessage = "OK";
+      response.header = "Content-Type: text/plain";
+      response.write(`Hello, ${userName}`);
+      response.end();
+
+      return;
+    }
+
+    response.statusCode = 500;
+    response.statusMessage = "error";
     response.end();
 
     return;
   }
 
-  if (userName) {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain";
-    response.write(`Hello, ${userName}`);
-    response.end();
-
-    return;
-  }
-
-  if (request.url === "/?hello") {
-    response.status = 400;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain";
-    response.write("Enter a name");
-    response.end();
-
-    return;
-  }
-
-  response.status = 200;
+  response.statusCode = 200;
   response.statusMessage = "OK";
   response.header = "Content-Type: text/plain";
   response.write("Hello world!");
   response.end();
 });
 server.listen(port, () => {
-  console.log(`Сервер запущен по адресу http://127.0.0.1:${3003}`);
+  console.log(`Сервер запущен по адресу http://127.0.0.1:${port}`);
 });
